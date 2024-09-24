12585ffaadab69af8abe33cdaa962269a03008da
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-09-24 22:27:59 +0300

    add 03.SumFirstLast

---
+++ 02.ArraysAndNestedArrays/01.ArraysAndNestedArraysLab/03.SumFirstLast.js
@@ -0,0 +1,9 @@
+function solve(input) {
+    const firstElement = Number(input[0]);
+    const lastElement = Number(input[input.length - 1]);
+
+    console.log(firstElement + lastElement);
+}
+
+solve(['20', '30', '40']);
+solve(['5', '10']);
