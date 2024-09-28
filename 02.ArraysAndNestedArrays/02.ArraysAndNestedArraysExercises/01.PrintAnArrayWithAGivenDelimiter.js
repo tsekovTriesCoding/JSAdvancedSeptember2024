cedfe36bb18edd3a08d8a28cfd907a91642febd3
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-09-28 21:15:35 +0300

    add 01.PrintAnArrayWithAGivenDelimiter

---
+++ 02.ArraysAndNestedArrays/02.ArraysAndNestedArraysExercises/01.PrintAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,29 @@
+function solve(array, delimiter) {
+    console.log(array.join(delimiter));
+}
+
+solve(['One',
+
+    'Two',
+
+    'Three',
+
+    'Four',
+
+    'Five'],
+
+    '-');
+
+solve(['How about no?',
+
+    'I',
+
+    'will',
+
+    'not',
+
+    'do',
+
+    'it!'],
+
+    '_');
