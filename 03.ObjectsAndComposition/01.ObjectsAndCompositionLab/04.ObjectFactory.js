c391f4c5ef8b99bf8854a978a63570aca4ef5a7c
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-10-04 16:43:49 +0300

    add 04.ObjectFactory

---
+++ 03.ObjectsAndComposition/01.ObjectsAndCompositionLab/04.ObjectFactory.js
@@ -0,0 +1,85 @@
+function solve() {
+    function factory(library, orders) {
+        let fulfilledOrders = [];
+
+        for (const order of orders) {
+            const name = order.template.name;
+            const parts = order.parts;
+
+            let fulfilledOrder = { name, };
+
+            parts.forEach(part => {
+                fulfilledOrder[part] = library[part];
+            });
+
+            fulfilledOrders.push(fulfilledOrder);
+        }
+
+        return fulfilledOrders;
+    }
+
+    const library = {
+
+        print: function () {
+
+            console.log(`${this.name} is printing a page`);
+
+        },
+
+        scan: function () {
+
+            console.log(`${this.name} is scanning a document`);
+
+        },
+
+        play: function (artist, track) {
+
+            console.log(`${this.name} is playing '${track}' by ${artist}`);
+
+        },
+
+    };
+
+    const orders = [
+
+        {
+
+            template: { name: 'ACME Printer' },
+
+            parts: ['print']
+
+        },
+
+        {
+
+            template: { name: 'Initech Scanner' },
+
+            parts: ['scan']
+
+        },
+
+        {
+
+            template: { name: 'ComTron Copier' },
+
+            parts: ['scan', 'print']
+
+        },
+
+        {
+
+            template: { name: 'BoomBox Stereo' },
+
+            parts: ['play']
+
+        }
+
+    ];
+
+    const products = factory(library, orders);
+    console.log(products);
+    console.log(Array.isArray(products));
+    
+}
+
+solve();
