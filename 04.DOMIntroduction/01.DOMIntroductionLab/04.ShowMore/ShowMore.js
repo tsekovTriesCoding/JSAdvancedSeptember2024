c97ff523c1faadedcd4f9a6c21ffe187730c56f0
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-10-08 18:03:59 +0300

    add04.ShowMore

---
+++ 04.DOMIntroduction/01.DOMIntroductionLab/04.ShowMore/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const readMore = document.getElementById('more');
+    const spanText = document.getElementById('text');
+
+    readMore.style.display = 'none';
+    spanText.style.display = 'inline';
+}
