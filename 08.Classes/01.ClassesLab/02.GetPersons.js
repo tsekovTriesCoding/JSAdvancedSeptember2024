9f8a62339232a9a73ff4350a06179d895b851037
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-10-21 21:24:22 +0300

    add 02.GetPersons

---
+++ 08.Classes/01.ClassesLab/02.GetPersons.js
@@ -0,0 +1,25 @@
+function solve() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    let persons = [];
+    const person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    const person2 = new Person('SoftUni');
+    const person3 = new Person('Stephan', 'Johnson', 25);
+    const person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    persons.push(person1, person2, person3, person4);
+    return persons;
+}
+
+solve();
